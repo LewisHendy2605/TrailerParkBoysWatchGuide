6619de257e50cae95d964b2da16a60ff41edae9e
LewisHendy2605 <lewishendy26@gmail.com>
2024-07-09 11:05:39 +0100

    pdated database + added watch link

--- src/components/MediaItemOverlay.js
+++ src/components/MediaItemOverlay.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../css/MediaItemOverlay.css";
+import { IoCloseCircleSharp } from "react-icons/io5";
 
 const MediaItemOverlay = ({ item, onClose }) => {
   return (
     <div className="overlay">
       <div className="overlay-content">
         <button className="close-button" onClick={onClose}>
-          X
+          <IoCloseCircleSharp />
         </button>
         <img
           src={`/TrailerParkBoysWatchGuide/images/${item.image_path}`}
@@ -17,6 +18,15 @@ const MediaItemOverlay = ({ item, onClose }) => {
           <h2>{item.title}</h2>
           <p>Year: {item.release_year}</p>
           <p>{item.desc}</p>
+          {item.watch_location && (
+            <a
+              href={item.watch_location}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Where to watch
+            </a>
+          )}
         </div>
       </div>
     </div>
